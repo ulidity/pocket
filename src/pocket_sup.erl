cc72a7c0ae4d0bd876e9d88ad02a20a38817ea79
Yauheni Tsiarokhin <yauheni@tsiarokhin.me>
2020-07-03 15:54:28 +0300

    add clock checker file base

--- src/pocket_sup.erl
+++ src/pocket_sup.erl
@@ -16,20 +16,11 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
+    SupFlags = #{
+      strategy => one_for_one, intensity => 1, period => 10
+    },
+    ChildSpecs = [#{
+      id => pocket_watch, start => {pocket_watch, start_link, []}
+    }],
     {ok, {SupFlags, ChildSpecs}}.
-
-%% internal functions
